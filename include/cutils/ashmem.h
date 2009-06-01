7be52b1a0e052635b361f9d8c186adbbcc981b7e
Bjorn Bringert <bringert@android.com>
2009-06-02 00:41:09 +0100

    Added ashmem_get_size_region() function.
    This is needed by the MemoryFile changes in
    https://android-git.corp.google.com/g/2714
    where it is used to find out whether a file descriptor
    refers to an ashmem region.

--- include/cutils/ashmem.h
+++ include/cutils/ashmem.h
@@ -18,6 +18,7 @@ int ashmem_create_region(const char *name, size_t size);
 int ashmem_set_prot_region(int fd, int prot);
 int ashmem_pin_region(int fd, size_t offset, size_t len);
 int ashmem_unpin_region(int fd, size_t offset, size_t len);
+int ashmem_get_size_region(int fd);
 
 #ifdef __cplusplus
 }
