b084929f5dd57b878f6debe6567a6c8888061fa0
Jean-Baptiste Queru <jbq@google.com>
2009-10-02 11:08:55 -0700

    Make AudioPara4 world-readable
    This way adb pull can get to it on user builds.
    
    BUG=1869158

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -159,7 +159,7 @@ static struct fs_path_config android_files[] = {
     { 00440, AID_BLUETOOTH, AID_BLUETOOTH, "system/etc/bluez/hcid.conf" },
     { 00440, AID_BLUETOOTH, AID_BLUETOOTH, "system/etc/bluez/input.conf" },
     { 00440, AID_BLUETOOTH, AID_BLUETOOTH, "system/etc/bluez/audio.conf" },
-    { 00440, AID_RADIO,     AID_AUDIO,     "system/etc/AudioPara4.csv" },
+    { 00444, AID_RADIO,     AID_AUDIO,     "system/etc/AudioPara4.csv" },
     { 00555, AID_ROOT,      AID_ROOT,      "system/etc/ppp/*" },
     { 00644, AID_SYSTEM,    AID_SYSTEM,    "data/app/*" },
     { 00644, AID_SYSTEM,    AID_SYSTEM,    "data/app-private/*" },
