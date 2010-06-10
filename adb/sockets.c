c419e2a210ad7e37a1f749f2ab4de7c7f95b90d7
André Goddard Rosa <andre.goddard@gmail.com>
2010-06-10 20:48:19 -0300

    adb: clean up 0 --> NULL for pointers
    Change-Id: I9216fdf534d807a55dfbc6800b5c3ef5f8c83368

--- adb/sockets.c
+++ adb/sockets.c
@@ -366,7 +366,7 @@ static void local_socket_event_func(int fd, unsigned ev, void *_s)
 asocket *create_local_socket(int fd)
 {
     asocket *s = calloc(1, sizeof(asocket));
-    if(s == 0) fatal("cannot allocate socket");
+    if (s == NULL) fatal("cannot allocate socket");
     install_local_socket(s);
     s->fd = fd;
     s->enqueue = local_socket_enqueue;
@@ -482,7 +482,7 @@ asocket *create_remote_socket(unsigned id, atransport *t)
     asocket *s = calloc(1, sizeof(aremotesocket));
     adisconnect*  dis = &((aremotesocket*)s)->disconnect;
 
-    if(s == 0) fatal("cannot allocate socket");
+    if (s == NULL) fatal("cannot allocate socket");
     s->id = id;
     s->enqueue = remote_socket_enqueue;
     s->ready = remote_socket_ready;
@@ -761,7 +761,7 @@ asocket *create_smart_socket(void (*action_cb)(asocket *s, const char *act))
 {
     D("Creating smart socket \n");
     asocket *s = calloc(1, sizeof(asocket));
-    if(s == 0) fatal("cannot allocate socket");
+    if (s == NULL) fatal("cannot allocate socket");
     s->id = 0;
     s->enqueue = smart_socket_enqueue;
     s->ready = smart_socket_ready;
