c31463d244d2e37d3329350a545e1f21f4f1e994
Mike Lockwood <lockwood@android.com>
2010-06-14 14:44:13 -0700

    Revert "adb: remove unnecessary include"
    This reverts commit e380c27e9fdca66fa2f4aa9fd599568c568fc33d.

--- adb/usb_linux.c
+++ adb/usb_linux.c
@@ -20,6 +20,7 @@
 #include <string.h>
 
 #include <usbhost/usbhost.h>
+#include <linux/usbdevice_fs.h>
 #include <linux/version.h>
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 20)
 #include <linux/usb/ch9.h>
