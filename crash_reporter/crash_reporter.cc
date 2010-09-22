563d08ba7db62d8d2cc85dc9275d2ee883d47c25
Thieu Le <thieule@chromium.org>
2010-09-21 17:07:05 -0700

    Fix build break.
    BUG=6446
    
    TEST=unit test, logging_UserCrash
    
    Change-Id: Iac9f5484d9218ef91ae606ef410409df5ec16382
    
    Review URL: http://codereview.chromium.org/3380022

--- crash_reporter/crash_reporter.cc
+++ crash_reporter/crash_reporter.cc
@@ -5,6 +5,7 @@
 #include <string>
 
 #include "base/file_util.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/string_util.h"
 #include "crash-reporter/kernel_collector.h"
@@ -143,6 +144,11 @@ int main(int argc, char *argv[]) {
   FilePath my_path(argv[0]);
   file_util::AbsolutePath(&my_path);
   s_metrics_lib.Init();
+  CommandLine::Init(argc, argv);
+  logging::InitLogging(NULL,
+                       logging::LOG_ONLY_TO_SYSTEM_DEBUG_LOG,
+                       logging::DONT_LOCK_LOG_FILE,
+                       logging::DELETE_OLD_LOG_FILE);
   s_system_log.Initialize(my_path.BaseName().value().c_str());
   KernelCollector kernel_collector;
   kernel_collector.Initialize(CountKernelCrash,
