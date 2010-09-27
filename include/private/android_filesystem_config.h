c29919c8d75ad218012daeda62e1e62200cad889
Mike Lockwood <lockwood@android.com>
2010-09-27 08:01:58 -0400

    Add AID_GPS user ID for GPS daemon
    Change-Id: I2f8ffba40e3f5145b37296f19c698e08d768ce59
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -52,6 +52,7 @@
 #define AID_VPN           1016  /* vpn system */
 #define AID_KEYSTORE      1017  /* keystore subsystem */
 #define AID_USB           1018  /* USB devices */
+#define AID_GPS           1021  /* GPS daemon */
 
 #define AID_SHELL         2000  /* adb and debug shell user */
 #define AID_CACHE         2001  /* cache access */
@@ -102,6 +103,7 @@ static const struct android_id_info android_ids[] = {
     { "vpn",       AID_VPN, },
     { "keystore",  AID_KEYSTORE, },
     { "usb",       AID_USB, },
+    { "gps",       AID_GPS, },
     { "inet",      AID_INET, },
     { "net_raw",   AID_NET_RAW, },
     { "net_admin", AID_NET_ADMIN, },
