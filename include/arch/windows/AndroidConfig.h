cfb61c272075e0fc25c39110b105825c128ac91f
Kenny Root <kroot@google.com>
2010-12-08 11:08:45 -0800

    Cygwin needs HAVE_OFF64_T and fix description
    During testing of compilation, it was discovered that Cygwin actually
    has off64_t.
    
    Also the description was wrong for HAVE_OFF64_T
    
    Change-Id: I6eb73d13da5a53fb03bf99ab7de82bfb6acab058

--- include/arch/windows/AndroidConfig.h
+++ include/arch/windows/AndroidConfig.h
@@ -177,9 +177,9 @@
 #define _LARGEFILE_SOURCE 1
 
 /*
- * Define if off_t is always 64-bit
+ * Define if platform has off64_t (and lseek64 and other xxx64 functions)
  */
-/* #define HAVE_OFF64_T */
+#define HAVE_OFF64_T
 
 /*
  * Defined if we have the backtrace() call for retrieving a stack trace.
