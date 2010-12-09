1c061b9ea952ebdf6093ca1e7c29bfff6332857e
Ying Wang <wangying@google.com>
2010-12-09 10:33:27 -0800

    dist for dist_files
    Change-Id: I665572a2212f1239b02b7816e37ada210afbdb48

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -50,7 +50,7 @@ endif
 LOCAL_STATIC_LIBRARIES := $(EXTRA_STATIC_LIBS) libzipfile libunz
 
 include $(BUILD_HOST_EXECUTABLE)
-$(call dist-for-goals,droid,$(LOCAL_BUILT_MODULE))
+$(call dist-for-goals,dist_files,$(LOCAL_BUILT_MODULE))
 
 ifeq ($(HOST_OS),linux)
 include $(CLEAR_VARS)
