d7a295221a96b02ebe50b65799da323770e67915
Bruce Beare <bruce.j.beare@intel.com>
2010-12-27 16:53:58 -0800

    Assembly coded android_memset16 and android_memset32
    Change-Id: Ifa05006df7ce7364d18eb10d64a818d619664475
    Author: Lu, Hongjiu <hongjiu.lu@intel.com>
    Signed-off-by: Mark Gross <mark.gross@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -112,12 +112,17 @@ LOCAL_MODULE := libcutils
 LOCAL_SRC_FILES := $(commonSources) ashmem-dev.c mq.c
 
 ifeq ($(TARGET_ARCH),arm)
-LOCAL_SRC_FILES += memset32.S
+LOCAL_SRC_FILES += arch-arm/memset32.S
 else  # !arm
 ifeq ($(TARGET_ARCH),sh)
 LOCAL_SRC_FILES += memory.c atomic-android-sh.c
 else  # !sh
+ifeq ($(TARGET_ARCH_VARIANT),x86-atom)
+LOCAL_CFLAGS += -DHAVE_MEMSET16 -DHAVE_MEMSET32
+LOCAL_SRC_FILES += arch-x86/android_memset16.S arch-x86/android_memset32.S memory.c
+else # !x86-atom
 LOCAL_SRC_FILES += memory.c
+endif # !x86-atom
 endif # !sh
 endif # !arm
 
