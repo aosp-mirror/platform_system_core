535164e9d9649a83d4d63829f3389f2bea339fe1
Mike Lockwood <lockwood@android.com>
2011-06-22 08:36:53 -0400

    Reimplement the "adb root" command to more closely match its previous behavior
    The adb gadget driver used to reset the USB bus when the adbd daemon exited,
    and the host side adb relied on this behavior to force it to reconnect
    with the new adbd instance after init relaunches it.  The new gadget
    drivers no longer automatically reset the USB bus when adbd is restarted
    which caused adb to hang since it was no longer forced to reconnect with the
    device.  We attempted to work around this on the host side adb, but that
    work around has not been reliable.
    
    This change adds a property trigger on the service.adb.root system property
    which will reset the USB bus and restart the adbd daemon when adbd sets
    the property to 1.  This should be much closer to the previous behavior
    and will hopefully fix some problems with automated testing.
    
    Change-Id: I177c37400009a3d83f21a5f9431f94fd1cc19b9b
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- adb/services.c
+++ adb/services.c
@@ -125,14 +125,12 @@ void restart_root_service(int fd, void *cookie)
             return;
         }
 
-        property_set("service.adb.root", "1");
         snprintf(buf, sizeof(buf), "restarting adbd as root\n");
         writex(fd, buf, strlen(buf));
         adb_close(fd);
 
-        // quit, and init will restart us as root
-        sleep(1);
-        exit(1);
+        // This will cause a property trigger in init.rc to restart us
+        property_set("service.adb.root", "1");
     }
 }
 
