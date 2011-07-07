697fb7622b402627e85081d2fbb84a8fc3daafbc
android-merger <android-build@android.com>
2011-07-07 14:52:12 -0700

    Revert "Validate sender credentials on netlink msg receive"
    This reverts commit b620a0b1c7ae486e979826200e8e441605b0a5d6.

--- libsysutils/src/NetlinkListener.cpp
+++ libsysutils/src/NetlinkListener.cpp
@@ -17,7 +17,6 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <linux/netlink.h>
 #include <string.h>
 
 #define LOG_TAG "NetlinkListener"
@@ -33,32 +32,10 @@ NetlinkListener::NetlinkListener(int socket) :
 bool NetlinkListener::onDataAvailable(SocketClient *cli)
 {
     int socket = cli->getSocket();
-    ssize_t count;
-    char cred_msg[CMSG_SPACE(sizeof(struct ucred))];
-    struct sockaddr_nl snl;
-    struct iovec iov = {mBuffer, sizeof(mBuffer)};
-    struct msghdr hdr = {&snl, sizeof(snl), &iov, 1, cred_msg, sizeof(cred_msg), 0};
+    int count;
 
-    if ((count = recvmsg(socket, &hdr, 0)) < 0) {
-        SLOGE("recvmsg failed (%s)", strerror(errno));
-        return false;
-    }
-
-    if ((snl.nl_groups != 1) || (snl.nl_pid != 0)) {
-        SLOGE("ignoring non-kernel netlink multicast message");
-        return false;
-    }
-
-    struct cmsghdr * cmsg = CMSG_FIRSTHDR(&hdr);
-
-    if (cmsg == NULL || cmsg->cmsg_type != SCM_CREDENTIALS) {
-        SLOGE("ignoring message with no sender credentials");
-        return false;
-    }
-
-    struct ucred * cred = (struct ucred *)CMSG_DATA(cmsg);
-    if (cred->uid != 0) {
-        SLOGE("ignoring message from non-root UID %d", cred->uid);
+    if ((count = recv(socket, mBuffer, sizeof(mBuffer), 0)) < 0) {
+        SLOGE("recv failed (%s)", strerror(errno));
         return false;
     }
 
