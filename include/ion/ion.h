830e19731dbfd5d8a2dd061f188f2ddc30443360
Dima Zavin <dima@android.com>
2012-02-27 16:33:03 -0800

    ion: fix header for including in C++ code
    Change-Id: I8fc10ac397e60d329a6887f0590d4dba6cbc7545
    Signed-off-by: Dima Zavin <dima@android.com>

--- include/ion/ion.h
+++ include/ion/ion.h
@@ -18,7 +18,12 @@
  *  limitations under the License.
  */
 
+#ifndef __SYS_CORE_ION_H
+#define __SYS_CORE_ION_H
+
 #include <linux/ion.h>
+
+__BEGIN_DECLS
 
 int ion_open();
 int ion_close(int fd);
@@ -30,3 +35,6 @@ int ion_map(int fd, struct ion_handle *handle, size_t length, int prot,
 int ion_share(int fd, struct ion_handle *handle, int *share_fd);
 int ion_import(int fd, int share_fd, struct ion_handle **handle);
 
+__END_DECLS
+
+#endif /* __SYS_CORE_ION_H */
