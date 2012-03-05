427d2b43c23c48aedadca008482f495145411e2b
Jean-Michel Trivi <jmtrivi@google.com>
2012-03-05 15:38:15 -0800

    Fix switch indentation in audio_bytes_per_sample
    Align "case" keyword with "switch" keyword.
    
    Change-Id: I57ef96253fb1fc925315b557ea27ccc35f63c2f8

--- include/system/audio.h
+++ include/system/audio.h
@@ -447,18 +447,18 @@ static inline size_t audio_bytes_per_sample(audio_format_t format)
     size_t size = 0;
 
     switch (format) {
-        case AUDIO_FORMAT_PCM_32_BIT:
-        case AUDIO_FORMAT_PCM_8_24_BIT:
-            size = sizeof(int32_t);
-            break;
-        case AUDIO_FORMAT_PCM_16_BIT:
-            size = sizeof(int16_t);
-            break;
-        case AUDIO_FORMAT_PCM_8_BIT:
-            size = sizeof(uint8_t);
-            break;
-        default:
-            break;
+    case AUDIO_FORMAT_PCM_32_BIT:
+    case AUDIO_FORMAT_PCM_8_24_BIT:
+        size = sizeof(int32_t);
+        break;
+    case AUDIO_FORMAT_PCM_16_BIT:
+        size = sizeof(int16_t);
+        break;
+    case AUDIO_FORMAT_PCM_8_BIT:
+        size = sizeof(uint8_t);
+        break;
+    default:
+        break;
     }
     return size;
 }
