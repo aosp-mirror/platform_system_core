9685194fc94510a33201aee9b80c23f206ccfe67
Jeff Sharkey <jsharkey@android.com>
2012-08-27 15:03:37 -0700

    File-system utilities for installd and zygote.
    Bug: 7008879
    Change-Id: Ia54b8d4ad434e8973f235e2a4728651814bef8a9

--- libcutils/multiuser.c
+++ libcutils/multiuser.c
@@ -16,14 +16,14 @@
 
 #include <cutils/multiuser.h>
 
-userid_t multiuser_getUserId(uid_t uid) {
+userid_t multiuser_get_user_id(uid_t uid) {
     return uid / MULTIUSER_APP_PER_USER_RANGE;
 }
 
-appid_t multiuser_getAppId(uid_t uid) {
+appid_t multiuser_get_app_id(uid_t uid) {
     return uid % MULTIUSER_APP_PER_USER_RANGE;
 }
 
-uid_t multiuser_getUid(userid_t userId, appid_t appId) {
+uid_t multiuser_get_uid(userid_t userId, appid_t appId) {
     return userId * MULTIUSER_APP_PER_USER_RANGE + (appId % MULTIUSER_APP_PER_USER_RANGE);
 }
