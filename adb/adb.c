d6d4286a28b4a9aef902585625ea6656ab06cf52
Jeff Sharkey <jsharkey@android.com>
2012-09-06 13:05:40 -0700

    Bring back ADB_EXTERNAL_STORAGE.
    Bug: 7119408
    Change-Id: Ic9a23fb6adfb1db771e1e278179586bca69a5edd

--- adb/adb.c
+++ adb/adb.c
@@ -1122,6 +1122,16 @@ int adb_main(int is_daemon, int server_port)
     if (auth_enabled)
         adb_auth_init();
 
+    // Our external storage path may be different than apps, since
+    // we aren't able to bind mount after dropping root.
+    const char* adb_external_storage = getenv("ADB_EXTERNAL_STORAGE");
+    if (NULL != adb_external_storage) {
+        setenv("EXTERNAL_STORAGE", adb_external_storage, 1);
+    } else {
+        D("Warning: ADB_EXTERNAL_STORAGE is not set.  Leaving EXTERNAL_STORAGE"
+          " unchanged.\n");
+    }
+
     /* don't listen on a port (default 5037) if running in secure mode */
     /* don't run as root if we are running in secure mode */
     if (should_drop_privileges()) {
