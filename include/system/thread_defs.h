000b1d0c5058d9da4dfec3a6f16b58fd50c34374
Alex Ray <aray@google.com>
2013-07-23 22:20:45 -0700

    thread_defs: remove trailing whitespace
    Change-Id: Idba19c64dfde67c40a98c29f68d5e8d4e555a4cf

--- include/system/thread_defs.h
+++ include/system/thread_defs.h
@@ -28,11 +28,11 @@ enum {
      * ***********************************************
      * ** Keep in sync with android.os.Process.java **
      * ***********************************************
-     * 
+     *
      * This maps directly to the "nice" priorities we use in Android.
      * A thread priority should be chosen inverse-proportionally to
      * the amount of work the thread is expected to do. The more work
-     * a thread will do, the less favorable priority it should get so that 
+     * a thread will do, the less favorable priority it should get so that
      * it doesn't starve the system. Threads not behaving properly might
      * be "punished" by the kernel.
      * Use the levels below when appropriate. Intermediate values are
@@ -42,26 +42,26 @@ enum {
 
     /* use for background tasks */
     ANDROID_PRIORITY_BACKGROUND     =  10,
-    
+
     /* most threads run at normal priority */
     ANDROID_PRIORITY_NORMAL         =   0,
-    
+
     /* threads currently running a UI that the user is interacting with */
     ANDROID_PRIORITY_FOREGROUND     =  -2,
 
     /* the main UI thread has a slightly more favorable priority */
     ANDROID_PRIORITY_DISPLAY        =  -4,
-    
+
     /* ui service treads might want to run at a urgent display (uncommon) */
     ANDROID_PRIORITY_URGENT_DISPLAY =  HAL_PRIORITY_URGENT_DISPLAY,
-    
+
     /* all normal audio threads */
     ANDROID_PRIORITY_AUDIO          = -16,
-    
+
     /* service audio threads (uncommon) */
     ANDROID_PRIORITY_URGENT_AUDIO   = -19,
 
-    /* should never be used in practice. regular process might not 
+    /* should never be used in practice. regular process might not
      * be allowed to use this level */
     ANDROID_PRIORITY_HIGHEST        = -20,
 
