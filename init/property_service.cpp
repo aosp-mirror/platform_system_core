348018a3a4fed316c0176dc2a6cce58a7c6a1e97
David Ng <dave@codeaurora.org>
2013-09-23 18:50:24 -0700

    init: Add vendor-specific initialization hooks.
    Allow optional vendor-specific initializations
    within init.  This can be used for runtime
    initialization setup that init rc scripts do
    not support.
    
    Change-Id: I7623a0d59b18f9ec8e3623958e2f7ccd72b877bf

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -73,6 +73,7 @@
 #include "subcontext.h"
 #include "system/core/init/property_service.pb.h"
 #include "util.h"
+#include "vendor_init.h"
 
 using namespace std::literals;
 
@@ -1096,6 +1097,9 @@ void PropertyLoadBootDefaults() {
                        << "' while loading .prop files" << error;
         }
     }
+
+    // Update with vendor-specific property runtime overrides
+    vendor_load_properties();
 
     property_initialize_ro_product_props();
     property_initialize_build_id();
