d9cd84277fb887924ecf355794537d884aca8424
Aaron Gable <agable@google.com>
2013-09-27 18:35:54 -0700

    Rewrite git/gerrit/gerrit-int urls to googlesource
    BUG=chromium:281665,chromium:290413
    
    TEST=None
    
    Change-Id: I114bcb152e1bf1823ee49134e0f7f51719b21944
    Reviewed-on: https://chromium-review.googlesource.com/171253
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- crash_reporter/common.mk
+++ crash_reporter/common.mk
@@ -7,7 +7,7 @@
 #
 # NOTE NOTE NOTE
 #  The authoritative common.mk is located in:
-#    http://git.chromium.org/gitweb/?p=chromiumos/platform/common-mk.git
+#    https://chromium.googlesource.com/chromiumos/platform/common-mk.git
 #  Please make all changes there, then copy into place in other repos.
 # NOTE NOTE NOTE
 #
