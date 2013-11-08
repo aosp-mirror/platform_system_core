03c932dffa2f72ee63de24c221d07d249a6eafd3
Colin Cross <ccross@android.com>
2013-11-07 20:11:37 -0800

    libion: move ion.h into local include directory
    Export it to users of libion with LOCAL_C_EXPORT_INCLUDE_DIRS
    
    Change-Id: I4d00a9dd2e99953723994d0304ca77e0ad50a3a9

--- libion/Android.mk
+++ libion/Android.mk
@@ -5,11 +5,14 @@ LOCAL_SRC_FILES := ion.c
 LOCAL_MODULE := libion
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ion.c ion_test.c
 LOCAL_MODULE := iontest
 LOCAL_MODULE_TAGS := optional tests
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_SHARED_LIBRARIES := liblog
 include $(BUILD_EXECUTABLE)
