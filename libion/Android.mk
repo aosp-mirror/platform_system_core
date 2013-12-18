dd6770bd49bcb0ddb0f58191dfc1a0e341cc97ac
Colin Cross <ccross@android.com>
2013-12-18 15:39:27 -0800

    libion: update to latest uapi header
    Pull the uapi headers from android-3.10 directly into libion.
    No other modules should be directly calling ion kernel
    interfaces, so there is no reason to have those interfaces
    exposed.
    
    Change-Id: I2d3fef08f6bb381522ecf7bcffea2b8280d5e737

--- libion/Android.mk
+++ libion/Android.mk
@@ -5,14 +5,14 @@ LOCAL_SRC_FILES := ion.c
 LOCAL_MODULE := libion
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/kernel-headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include $(LOCAL_PATH)/kernel-headers
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ion.c ion_test.c
 LOCAL_MODULE := iontest
 LOCAL_MODULE_TAGS := optional tests
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/kernel-headers
 LOCAL_SHARED_LIBRARIES := liblog
 include $(BUILD_EXECUTABLE)
