bfa7d0822b2811d667fba1b326fd54a083fd5a95
Elliott Hughes <enh@google.com>
2014-01-07 18:20:17 -0800

    SWAP_FLAG_* constants are now in <sys/swap.h>.
    Bug: 9336527
    Change-Id: I77e2b05cefbfb4eebda39b4079107833f4160563

--- toolbox/swapon.c
+++ toolbox/swapon.c
@@ -5,12 +5,6 @@
 #include <asm/page.h>
 #include <sys/swap.h>
 
-/* XXX These need to be obtained from kernel headers. See b/9336527 */
-#define SWAP_FLAG_PREFER        0x8000
-#define SWAP_FLAG_PRIO_MASK     0x7fff
-#define SWAP_FLAG_PRIO_SHIFT    0
-#define SWAP_FLAG_DISCARD       0x10000
-
 void usage(char *name)
 {
     fprintf(stderr, "Usage: %s [-p prio] <filename>\n"
