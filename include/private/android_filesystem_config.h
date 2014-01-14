7baebf1345ff9e861ea8ccf9712128a3b7cd9fcc
Doug Zongker <dougz@android.com>
2014-01-14 12:30:40 -0800

    add the pre-recovery service
    Add a service called "pre-recovery" which is normally stopped but can
    be started by the system server when we want to go into recovery.  It
    will do any preparation needed (currently needed to handle update
    packages that reside on an encrypted /data partition) and then set
    sys.powerctl when it's ready to actually reboot.
    
    Bug: 12188746
    Change-Id: I894a4cb200395a0f6d7fe643ed4c2ac0a45d2052

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -247,6 +247,7 @@ static const struct fs_path_config android_files[] = {
     /* the following files have enhanced capabilities and ARE included in user builds. */
     { 00750, AID_ROOT,      AID_SHELL,     (1 << CAP_SETUID) | (1 << CAP_SETGID), "system/bin/run-as" },
 
+    { 00750, AID_ROOT,      AID_ROOT,      0, "system/bin/uncrypt" },
     { 00755, AID_ROOT,      AID_SHELL,     0, "system/bin/*" },
     { 00755, AID_ROOT,      AID_ROOT,      0, "system/lib/valgrind/*" },
     { 00755, AID_ROOT,      AID_SHELL,     0, "system/xbin/*" },
