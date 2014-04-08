d8f2a8d34a5655d81fec2f56eb3ec36bd508886a
Igor Murashkin <iam@google.com>
2014-04-08 11:22:42 -0700

    cutils: Add property_get_bool, _get_int32, _get_int64
    * Read out system properties with same syntax as SystemProperties.java
    * Also adds unit test suite to validate correctness of properties
    * Also fixes buffer overrun in property_get
    
    (cherry picked from commit d4507e9246e4855c5431cac5c3d1a9155caebc87)
    
    Change-Id: Ifd42911f93e17da09e6ff1298e8875e02f3b6608

---
+++ libcutils/tests/Android.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+test_src_files := \
+    PropertiesTest.cpp \
+
+shared_libraries := \
+    libutils
+
+static_libraries := \
+    libcutils
+
+LOCAL_SHARED_LIBRARIES := $(shared_libraries)
+LOCAL_STATIC_LIBRARIES := $(static_libraries)
+LOCAL_SRC_FILES := $(test_src_files)
+LOCAL_MODULE := libcutils_test
+include $(BUILD_NATIVE_TEST)
