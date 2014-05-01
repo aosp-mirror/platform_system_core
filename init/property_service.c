ebf49f3e875981e9037ce36eddcab3ccb5925ebf
Mark Salyzyn <salyzyn@google.com>
2014-05-01 08:34:26 -0700

    init: 64-bit build issue
    (cherry pick from Icc5344abc184adf4247175d85b2da08af4636e49)
    
    Change-Id: I88b0c9b20720ac62ebae8359ded4ab77b4ebcdde

--- init/property_service.c
+++ init/property_service.c
@@ -557,7 +557,8 @@ static void load_persistent_properties()
                     || (sb.st_gid != 0)
                     || (sb.st_nlink != 1)) {
                 ERROR("skipping insecure property file %s (uid=%u gid=%u nlink=%d mode=%o)\n",
-                      entry->d_name, sb.st_uid, sb.st_gid, sb.st_nlink, sb.st_mode);
+                      entry->d_name, (unsigned int)sb.st_uid, (unsigned int)sb.st_gid,
+                      sb.st_nlink, sb.st_mode);
                 close(fd);
                 continue;
             }
