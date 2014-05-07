7d7f5e58db76fb2c437e6a6fb0f196004be604a8
Nick Kralevich <nnk@google.com>
2014-05-07 16:56:42 -0700

    Make lmkd dynamically linked
    Statically linked binaries cannot take advantage of ASLR,
    making them less secure.
    
    In addition, statically linked binaries consume more ram,
    because shared libraries cannot be reused.
    
      executable size before: 87728
      executable size after:  13656
    
    Change-Id: I9d02d865f9beeaaaadcd5009f64ac015931d4b11

--- lmkd/Android.mk
+++ lmkd/Android.mk
@@ -2,8 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lmkd.c
-LOCAL_STATIC_LIBRARIES := libcutils liblog libm libc
-LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_SHARED_LIBRARIES := libcutils liblog libm libc
 LOCAL_CFLAGS := -Werror
 
 LOCAL_MODULE := lmkd
