5d9d434efadf1c535c7fea634d5306e18c68ef1f
Jeff Sharkey <jsharkey@android.com>
2014-05-26 18:30:43 -0700

    Add "exec" service: shell commands with no pty.
    To facilitate device scripts that want to read/write binary data from
    the host side, this change introduces a new "exec" service that
    behaves like "shell" but without creating a pty, which would otherwise
    mangle binary data.
    
    After forking, it hooks up stdin/stdout of the child process to
    the socket connected through to the host.  The adb transport doesn't
    support shutdown(), so the host can't half-close the socket and wait
    for device termination.  Instead, the host side now has two explicit
    commands "exec-in" and "exec-out" for either sending or receiving
    data.
    
    Teach host side copy_to_file() to deal with stdin/stdout special
    cases.  Switch device side backup/restore services to use the new
    create_subproc_raw under the hood.
    
    Change-Id: I5993049803519d3959761f2363037b02c50920ee

--- adb/adb.h
+++ adb/adb.h
@@ -324,11 +324,6 @@ int       create_jdwp_connection_fd(int  jdwp_pid);
 #endif
 
 #if !ADB_HOST
-typedef enum {
-    BACKUP,
-    RESTORE
-} BackupOperation;
-int backup_service(BackupOperation operation, char* args);
 void framebuffer_service(int fd, void *cookie);
 void remount_service(int fd, void *cookie);
 #endif
@@ -416,7 +411,7 @@ void adb_qemu_trace(const char* fmt, ...);
 #  define  D(...)          ((void)0)
 #  define  DR(...)         ((void)0)
 #  define  ADB_TRACING     0
-#endif
+#endif /* ADB_TRACE */
 
 
 #if !DEBUG_PACKETS
@@ -473,6 +468,11 @@ int connection_state(atransport *t);
 
 extern int HOST;
 extern int SHELL_EXIT_NOTIFY_FD;
+
+typedef enum {
+    SUBPROC_PTY = 0,
+    SUBPROC_RAW = 1,
+} subproc_mode;
 
 #define CHUNK_SIZE (64*1024)
 
