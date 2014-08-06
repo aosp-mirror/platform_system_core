678bd2e8225ccebe678430d4095b7fc78253bcf8
Alexander Ivchenko <alexander.ivchenko@intel.com>
2014-08-06 14:51:40 +0400

    Fix build of adb with gcc-4.9.
    Without this patch we get:
    
    system/core/adb/commandline.c:1629:16: error: array subscript is above
    array bounds [-Werror=array-bounds]
    
    Signed-off-by: Alexander Ivchenko <alexander.ivchenko@intel.com>
    
    (cherry picked from commit 53723338858772a0189aa3f92f4d4bbdaa9b6772)
    
    Change-Id: I920c1de933ce5ba0a0d57eb8a9b557325a767a2a

--- adb/commandline.c
+++ adb/commandline.c
@@ -1746,9 +1746,10 @@ top:
     return 1;
 }
 
+#define MAX_ARGV_LENGTH 16
 static int do_cmd(transport_type ttype, char* serial, char *cmd, ...)
 {
-    char *argv[16];
+    char *argv[MAX_ARGV_LENGTH];
     int argc;
     va_list ap;
 
@@ -1765,7 +1766,9 @@ static int do_cmd(transport_type ttype, char* serial, char *cmd, ...)
     }
 
     argv[argc++] = cmd;
-    while((argv[argc] = va_arg(ap, char*)) != 0) argc++;
+    while(argc < MAX_ARGV_LENGTH &&
+        (argv[argc] = va_arg(ap, char*)) != 0) argc++;
+    assert(argc < MAX_ARGV_LENGTH);
     va_end(ap);
 
 #if 0
