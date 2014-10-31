2b8852dececd0ff681e08fe2127d3defb4f73578
Michael Lentine <mlentine@google.com>
2014-10-31 15:22:52 -0700

    Fix native_handle_create to check if malloc fails
    Bug: 18076253
    Change-Id: I8dbe3af88f52f18f74a3ab857fea53ae7585e2f7

--- libcutils/native_handle.c
+++ libcutils/native_handle.c
@@ -30,9 +30,11 @@ native_handle_t* native_handle_create(int numFds, int numInts)
     native_handle_t* h = malloc(
             sizeof(native_handle_t) + sizeof(int)*(numFds+numInts));
 
-    h->version = sizeof(native_handle_t);
-    h->numFds = numFds;
-    h->numInts = numInts;
+    if (h) {
+        h->version = sizeof(native_handle_t);
+        h->numFds = numFds;
+        h->numInts = numInts;
+    }
     return h;
 }
 
