83d85563c51fa39cb61b836302a8c26fe0e2ace6
Nick Kralevich <nnk@google.com>
2014-11-19 13:33:22 -0800

    logd: throttle SELinux denials to 20/sec
    Impose a limit of 20 selinux denials per second. Denials beyond
    that point don't add any value, and have the potential to cause
    crashes or denial of service attacks.
    
    Do some other misc cleanup while I'm here.
    
    Bug: 18341932
    
    (cherry picked from commit c234a1b879d9c9d8e1a797c5dcf3098249945748)
    
    Change-Id: Ic5c96003db82e504baf301e87be5c8c539c73c2a

--- logd/libaudit.h
+++ logd/libaudit.h
@@ -37,11 +37,6 @@ typedef enum {
     GET_REPLY_NONBLOCKING
 } reply_t;
 
-typedef enum {
-    WAIT_NO,
-    WAIT_YES
-} rep_wait_t;
-
 /* type == AUDIT_SIGNAL_INFO */
 struct audit_sig_info {
     uid_t uid;
@@ -92,12 +87,10 @@ extern int  audit_get_reply(int fd, struct audit_message *rep, reply_t block,
  *  The fd returned by a call to audit_open()
  * @param pid
  *  The pid whom to set as the reciever of audit messages
- * @param wmode
- *  Whether or not to block on the underlying socket io calls.
  * @return
  *  This function returns 0 on success, -errno on error.
  */
-extern int  audit_set_pid(int fd, uint32_t pid, rep_wait_t wmode);
+extern int  audit_setup(int fd, uint32_t pid);
 
 __END_DECLS
 
