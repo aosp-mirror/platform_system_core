19db38c101dfe7aa1d3e5f04fe6f978e50043477
Elliott Hughes <enh@google.com>
2015-03-16 20:36:41 -0700

    Remove netcfg.
    Change-Id: Ia76fcb45ddd09fdc2ba1fb986c65dc954b478284

--- netcfg/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= netcfg.c
-LOCAL_MODULE:= netcfg
-LOCAL_SHARED_LIBRARIES := libnetutils
-LOCAL_CFLAGS := -Werror
-include $(BUILD_EXECUTABLE)
