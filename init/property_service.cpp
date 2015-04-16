95f709a9f59ce79c65a9419d4227e877f3780ec7
Elliott Hughes <enh@google.com>
2015-04-15 17:05:23 -0700

    Don't try to read /system/default.prop.
    It doesn't exist. We use /system/build.prop.
    
    Change-Id: Ie635f9b3cc65de807f128339199b853db5a8eb2f

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -508,7 +508,6 @@ void load_persist_props(void) {
 
 void load_all_props() {
     load_properties_from_file(PROP_PATH_SYSTEM_BUILD, NULL);
-    load_properties_from_file(PROP_PATH_SYSTEM_DEFAULT, NULL);
     load_properties_from_file(PROP_PATH_VENDOR_BUILD, NULL);
     load_properties_from_file(PROP_PATH_BOOTIMAGE_BUILD, NULL);
     load_properties_from_file(PROP_PATH_FACTORY, "ro.*");
