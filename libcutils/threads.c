cd206b3900639c44767cd892bccfbffabf222f67
Dan Albert <danalbert@google.com>
2015-04-29 17:13:32 -0700

    Fix gettid() on Windows.
    Accidentally had this all hidden by an #ifndef _WIN32 when I wrote it.
    
    Bug: 19517541
    
    (cherry picked from commit b3a36ca5ee79411c18984b85b224d9285b4468af)
    
    Change-Id: Ifbd5d19e506e7313700e2e29a2dae5736e049844

--- libcutils/threads.c
+++ libcutils/threads.c
@@ -16,8 +16,6 @@
 
 #include "cutils/threads.h"
 
-#if !defined(_WIN32)
-
 // For gettid.
 #if defined(__APPLE__)
 #include "AvailabilityMacros.h"  // For MAC_OS_X_VERSION_MAX_ALLOWED
@@ -30,8 +28,28 @@
 #include <syscall.h>
 #include <unistd.h>
 #elif defined(_WIN32)
-#include <Windows.h>
+#include <windows.h>
 #endif
+
+// No definition needed for Android because we'll just pick up bionic's copy.
+#ifndef __ANDROID__
+pid_t gettid() {
+#if defined(__APPLE__)
+  uint64_t owner;
+  int rc = pthread_threadid_np(NULL, &owner);
+  if (rc != 0) {
+    abort();
+  }
+  return owner;
+#elif defined(__linux__)
+  return syscall(__NR_gettid);
+#elif defined(_WIN32)
+  return GetCurrentThreadId();
+#endif
+}
+#endif  // __ANDROID__
+
+#if !defined(_WIN32)
 
 void*  thread_store_get( thread_store_t*  store )
 {
@@ -57,24 +75,6 @@ extern void   thread_store_set( thread_store_t*          store,
 
     pthread_setspecific( store->tls, value );
 }
-
-// No definition needed for Android because we'll just pick up bionic's copy.
-#ifndef __ANDROID__
-pid_t gettid() {
-#if defined(__APPLE__)
-  uint64_t owner;
-  int rc = pthread_threadid_np(NULL, &owner);
-  if (rc != 0) {
-    abort();
-  }
-  return owner;
-#elif defined(__linux__)
-  return syscall(__NR_gettid);
-#elif defined(_WIN32)
-  return (pid_t)GetCurrentThreadId();
-#endif
-}
-#endif  // __ANDROID__
 
 #else /* !defined(_WIN32) */
 void*  thread_store_get( thread_store_t*  store )
