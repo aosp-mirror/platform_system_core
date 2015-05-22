0c3f1ae66b693ef0f37066b697cc10a07ef56acc
Christopher Ferris <cferris@google.com>
2015-05-22 14:26:13 -0700

    Refactor dump_memory function.
    - Add dumping memory around registers for x86/x86_64.
    - Add unit tests for new dump_memory function.
    - Cleanup all of the machine.cpp files.
    - Increase the high address check for 32 bit, and decrease the high
      address allowed for 64 bit slightly to match mips64.
    
    Bug: 21206576
    (cherry picked from commit e8bc77eb845ab5557a4c98fe0da604d4a3740bef)
    
    Change-Id: I49ec237e30076a232f084da1072bf9aba15dc0cd

--- debuggerd/test/log_fake.h
+++ debuggerd/test/log_fake.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,13 @@
  * limitations under the License.
  */
 
-#ifndef _DEBUGGERD_MACHINE_H
-#define _DEBUGGERD_MACHINE_H
+#ifndef _DEBUGGERD_TEST_LOG_FAKE_H
+#define _DEBUGGERD_TEST_LOG_FAKE_H
 
-#include <sys/types.h>
+#include <string>
 
-#include <backtrace/Backtrace.h>
+void resetLogs();
+std::string getFakeLogBuf();
+std::string getFakeLogPrint();
 
-#include "utility.h"
-
-void dump_memory_and_code(log_t* log, Backtrace* backtrace);
-void dump_registers(log_t* log, pid_t tid);
-
-#endif // _DEBUGGERD_MACHINE_H
+#endif // _DEBUGGERD_TEST_LOG_FAKE_H
