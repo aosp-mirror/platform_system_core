d815178b7512cb44d8b5f234e3f823b5a3e44dea
Paul Lawrence <paullawrence@google.com>
2015-07-01 14:40:56 -0700

    Change init sequence to support file level encryption
    File level encryption must get the key between mounting userdata and
    calling post_fs_data when the directories are created. This requires
    access to keymaster, which in turn is found from a system property.
    
    Split property loaded into system and data, and load in right order.
    
    Bug: 22233063
    Change-Id: I8a6c40d44e17de386417a443c9dfc3b4e7fe59a5

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -560,16 +560,10 @@ void load_recovery_id_prop() {
     close(fd);
 }
 
-void load_all_props() {
+void load_system_props() {
     load_properties_from_file(PROP_PATH_SYSTEM_BUILD, NULL);
     load_properties_from_file(PROP_PATH_VENDOR_BUILD, NULL);
     load_properties_from_file(PROP_PATH_FACTORY, "ro.*");
-
-    load_override_properties();
-
-    /* Read persistent properties after all default values have been loaded. */
-    load_persistent_properties();
-
     load_recovery_id_prop();
 }
 
