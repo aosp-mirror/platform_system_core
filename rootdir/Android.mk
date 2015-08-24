0ea4ffdc0658982a0744de1da24c781035015830
Tom Cherry <tomcherry@google.com>
2015-08-24 13:56:43 -0700

    remove TARGET_PROVIDES_INIT_RC
    Change-Id: I15105a8dc11e218d95d4f5a4cee5b2a166d3d639

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,8 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 
 #######################################
 # init.rc
-# Only copy init.rc if the target doesn't have its own.
-ifneq ($(TARGET_PROVIDES_INIT_RC),true)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := init.rc
@@ -12,7 +10,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 
 include $(BUILD_PREBUILT)
-endif
 
 #######################################
 # asan.options
