b75cce0389748bea111ca62af623645117e12d9d
Mark Salyzyn <salyzyn@google.com>
2015-11-30 11:35:56 -0800

    logd: wakeup on wrap or timeout
    If a timeout is specified for the reader, then go to sleep
    with the socket open. If the start time is about to get
    pruned in the specified log buffers, then wakeup and dump
    the logs; or wakeup on timeout, whichever comes first.
    
    Bug: 25929746
    Change-Id: I7d2421c2c5083b33747b84f74d9a560d3ba645df

--- logd/LogReader.cpp
+++ logd/LogReader.cpp
@@ -67,6 +67,14 @@ bool LogReader::onDataAvailable(SocketClient *cli) {
         start.strptime(cp + sizeof(_start) - 1, "%s.%q");
     }
 
+    uint64_t timeout = 0;
+    static const char _timeout[] = " timeout=";
+    cp = strstr(buffer, _timeout);
+    if (cp) {
+        timeout = atol(cp + sizeof(_timeout) - 1) * NS_PER_SEC +
+                  log_time(CLOCK_REALTIME).nsec();
+    }
+
     unsigned int logMask = -1;
     static const char _logIds[] = " lids=";
     cp = strstr(buffer, _logIds);
@@ -166,7 +174,7 @@ bool LogReader::onDataAvailable(SocketClient *cli) {
         }
     }
 
-    FlushCommand command(*this, nonBlock, tail, logMask, pid, sequence);
+    FlushCommand command(*this, nonBlock, tail, logMask, pid, sequence, timeout);
     command.runSocketCommand(cli);
     return true;
 }
