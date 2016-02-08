db262ffa62d789d4889ebe73abbb29fa0f373125
Josh Gao <jmgao@google.com>
2016-02-08 11:22:50 -0800

    adb: fix mkdirs test.
    The behavior of mkdirs was changed a while ago, without updating the
    test.
    
    Change-Id: I2aaa73818933b281e911c42a14e3c843d8bd972a
    (cherry picked from commit 1172b2ec71db29533231ddc4d979be98d7503f0b)

--- adb/adb_utils_test.cpp
+++ adb/adb_utils_test.cpp
@@ -112,7 +112,7 @@ TEST(adb_utils, adb_dirname) {
 }
 
 void test_mkdirs(const std::string basepath) {
-  EXPECT_TRUE(mkdirs(basepath));
+  EXPECT_TRUE(mkdirs(adb_dirname(basepath)));
   EXPECT_NE(-1, adb_creat(basepath.c_str(), 0600));
   EXPECT_FALSE(mkdirs(basepath + "/subdir/"));
 }
