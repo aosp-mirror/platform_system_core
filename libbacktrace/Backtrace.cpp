80088cf72ffcffe87a9195fce13321952acc2464
Josh Gao <jmgao@google.com>
2016-03-23 11:42:53 -0700

    Don't demangle symbol names.
    Bug: http://b/27299236
    Change-Id: I26ef47f80d4d6048a316ba51e83365ff65d70439
    (cherry picked from commit fbbc744130c85538a754d245e958d7d79f24309c)

--- libbacktrace/Backtrace.cpp
+++ libbacktrace/Backtrace.cpp
@@ -54,24 +54,8 @@ Backtrace::~Backtrace() {
   }
 }
 
-extern "C" char* __cxa_demangle(const char* mangled, char* buf, size_t* len,
-                                int* status);
-
 std::string Backtrace::GetFunctionName(uintptr_t pc, uintptr_t* offset) {
   std::string func_name = GetFunctionNameRaw(pc, offset);
-  if (!func_name.empty()) {
-#if defined(__APPLE__)
-    // Mac OS' __cxa_demangle demangles "f" as "float"; last tested on 10.7.
-    if (func_name[0] != '_') {
-      return func_name;
-    }
-#endif
-    char* name = __cxa_demangle(func_name.c_str(), 0, 0, 0);
-    if (name) {
-      func_name = name;
-      free(name);
-    }
-  }
   return func_name;
 }
 
