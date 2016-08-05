a90aa13b5b8783be8491095ae11c653e5df807b3
Greg Hackmann <ghackmann@google.com>
2016-08-05 10:07:28 -0700

    libsync: remove unused liblog dependency
    Change-Id: I69840ece3f13809b2440edbe6cfa412a56aee4aa
    Signed-off-by: Greg Hackmann <ghackmann@google.com>

--- libsync/Android.mk
+++ libsync/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sync.c
 LOCAL_MODULE := libsync
 LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_CFLAGS := -Werror
@@ -14,7 +13,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sync.c sync_test.c
 LOCAL_MODULE := sync_test
 LOCAL_MODULE_TAGS := optional tests
-LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_CFLAGS := -Werror
 include $(BUILD_EXECUTABLE)
