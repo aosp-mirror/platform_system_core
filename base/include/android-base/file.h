840b6019c035eb3abeadfb2dc8abac38692a829b
Dimitry Ivanov <dimitry@google.com>
2016-09-09 10:49:21 -0700

    Add android::base::Realpath.
    Bug: http://b/31396973
    Test: libbase_test on host and device
    Change-Id: I1e5f15c76227ec1c2128baa38eb454d347987703

--- base/include/android-base/file.h
+++ base/include/android-base/file.h
@@ -47,6 +47,7 @@ bool WriteFully(int fd, const void* data, size_t byte_count);
 bool RemoveFileIfExists(const std::string& path, std::string* err = nullptr);
 
 #if !defined(_WIN32)
+bool Realpath(const std::string& path, std::string* result);
 bool Readlink(const std::string& path, std::string* result);
 #endif
 
