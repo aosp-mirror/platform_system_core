42e802c2fb6a637d77dc4cc4a754c1fd39af8802
Keun-young Park <keunyoung@google.com>
2016-10-28 12:05:36 -0700

    Add UID for vehicle network
    - Access to vehicle network is done in vehicle network service process.
    
    Test: build
    bug: 32508433
    Change-Id: I340d1f4964d70b53f10d84fb15a62107e1dedc0e

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -95,6 +95,7 @@
 #define AID_DNS           1051  /* DNS resolution daemon (system: netd) */
 #define AID_DNS_TETHER    1052  /* DNS resolution daemon (tether: dnsmasq) */
 #define AID_WEBVIEW_ZYGOTE 1053 /* WebView zygote process */
+#define AID_VEHICLE_NETWORK 1054 /* Vehicle network service */
 /* Changes to this file must be made in AOSP, *not* in internal branches. */
 
 #define AID_SHELL         2000  /* adb and debug shell user */
@@ -208,6 +209,7 @@ static const struct android_id_info android_ids[] = {
     { "dns",           AID_DNS, },
     { "dns_tether",    AID_DNS_TETHER, },
     { "webview_zygote", AID_WEBVIEW_ZYGOTE, },
+    { "vehicle_network", AID_VEHICLE_NETWORK, },
 
     { "shell",         AID_SHELL, },
     { "cache",         AID_CACHE, },
