021d60be98ca2eb72b2c63e3b1fea2169b7c2b69
Tim Murray <timmurray@google.com>
2016-11-02 13:23:25 -0700

    libsuspend: remove autosleep and earlysuspend
    autosleep and earlysuspend are long-deprecated, so remove them to make
    libsuspend slightly more comprehensible.
    
    Test: builds and still suspends on marlin
    
    bug 32621856
    
    Change-Id: I66a904c3fce0111fb5e9d1ab9d205c97617ae210

--- libsuspend/autosuspend.c
+++ libsuspend/autosuspend.c
@@ -34,19 +34,6 @@ static int autosuspend_init(void)
         return 0;
     }
 
-    autosuspend_ops = autosuspend_earlysuspend_init();
-    if (autosuspend_ops) {
-        goto out;
-    }
-
-/* Remove autosleep so userspace can manager suspend/resume and keep stats */
-#if 0
-    autosuspend_ops = autosuspend_autosleep_init();
-    if (autosuspend_ops) {
-        goto out;
-    }
-#endif
-
     autosuspend_ops = autosuspend_wakeup_count_init();
     if (autosuspend_ops) {
         goto out;
