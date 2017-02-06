e63f366ee32667fb7c32513c9770fa24bac1fcb4
Todd Poynor <toddpoynor@google.com>
2017-02-06 15:43:05 -0800

    android_reboot: move reboot reason file to data/misc/reboot/
    Move last reboot reason file to new directory data/misc/reboot/ to
    require only SELinux permissions specific to this new file.
    
    Bug: 30994946
    Test: manual: reboot command, setprop sys.powerctl
    Change-Id: I1e067235aa4b06391cff8ab0741a9d317ba5b7da

--- libcutils/include/cutils/android_reboot.h
+++ libcutils/include/cutils/android_reboot.h
@@ -31,7 +31,7 @@ __BEGIN_DECLS
 #define ANDROID_RB_PROPERTY "sys.powerctl"
 
 /* Android reboot reason stored in this file */
-#define LAST_REBOOT_REASON_FILE "/data/misc/recovery/last_reboot_reason"
+#define LAST_REBOOT_REASON_FILE "/data/misc/reboot/last_reboot_reason"
 
 int android_reboot(int cmd, int flags, const char *arg);
 int android_reboot_with_callback(
