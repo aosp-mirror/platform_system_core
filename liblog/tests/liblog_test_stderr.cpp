57075240038be316a6a08a5e4a627ddf73740b4a
Mark Salyzyn <salyzyn@google.com>
2017-03-17 10:29:41 -0700

    liblog: test: *.android_errorWriteWithinfoLog__android_logger_list_read__*
    We would experience failures as test runs interfere with each other.
    Create a unique tag for each test run signature.  Switch from using
    TEST_PREFIX to TEST_LOGGER to identify the logger transport being
    inspected and make that part of the signature.  Make sure 32 bit and
    64 bit tests do not interfere.
    
    Test: gTest liblog-unit-tests
          cts-tradefed run cts -m CtsLiblogTestCases
    Bug: 36232924
    Change-Id: I4d58242e5ef8e68e2d4b27cecf538938e17acf3f

--- liblog/tests/liblog_test_stderr.cpp
+++ liblog/tests/liblog_test_stderr.cpp
@@ -1,5 +1,5 @@
 #include <log/log_transport.h>
 #define liblog liblog_stderr
-#define TEST_PREFIX android_set_log_transport(LOGGER_STDERR);
+#define TEST_LOGGER LOGGER_STDERR
 #define USING_LOGGER_STDERR
 #include "liblog_test.cpp"
--- liblog/tests/liblog_test_stderr_local.cpp
+++ liblog/tests/liblog_test_stderr_local.cpp
@@ -1,4 +1,4 @@
 #include <log/log_transport.h>
 #define liblog liblog_stderr_local
-#define TEST_PREFIX android_set_log_transport(LOGGER_LOCAL | LOGGER_STDERR);
+#define TEST_LOGGER (LOGGER_LOCAL | LOGGER_STDERR)
 #include "liblog_test.cpp"
