7726e439dbc777ba6fa5ad40162a4b4c9a2e33ea
Simao Gomes Viana <xdevs23@outlook.com>
2017-04-16 00:07:53 +0200

    Filter GalleryDatab*
    Change-Id: I11e5aae86edd61cafd23daf7c7a086e98e5cc57d
    
    Former-commit-id: 7a2f85d78327f55306f476a7e4c527b7b4dbdd2f
    Former-commit-id: 7fd04e30a927966ce8d89e14bc86a98760e37115
    Former-commit-id: 19fd2bd2c559dfaf6a79b1986434b77fdd89c8e3
    Former-commit-id: 2177c4d0ae53846d26129513e17d5ae7bad0e369
    Former-commit-id: e5af0ce8b9743ac2d83ff4c73357272eabe3d434

--- liblog/logprint.c
+++ liblog/logprint.c
@@ -201,7 +201,7 @@ static android_LogPriority filterPriForTag(AndroidLogFormat* p_format,
 LIBLOG_ABI_PUBLIC int android_log_shouldPrintLine(AndroidLogFormat* p_format,
                                                   const char* tag,
                                                   android_LogPriority pri) {
-    if (!strncmp(tag, "AEC_PORT", 8) || !strncmp(tag, "mm-camera", 9))
+    if (!strncmp(tag, "AEC_PORT", 8) || !strncmp(tag, "mm-camera", 9) || !strncmp(tag, "GalleryDatab", 12))
         return 0;
     else
         return pri >= filterPriForTag(p_format, tag);
