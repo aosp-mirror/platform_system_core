3ac3c02b258ac9f9fb794506b519c1beed075bd5
Tom Cherry <tomcherry@google.com>
2017-05-01 14:16:41 -0700

    init: add an initializer for keychord_id_
    Add unit test to ensure all POD types of Service are initialized.
    
    Bug: 37855222
    Test: Ensure bugreport is triggered via keychord properly.
    Test: New unit tests
    Merged-In: If2cfea15a74ab417a7b909a60c264cb8eb990de7
    Change-Id: If2cfea15a74ab417a7b909a60c264cb8eb990de7
    (cherry picked from commit 7da548578c25683fe0082283303e16961df312da)

--- init/Android.mk
+++ init/Android.mk
@@ -142,6 +142,7 @@ LOCAL_MODULE := init_tests
 LOCAL_SRC_FILES := \
     init_parser_test.cpp \
     property_service_test.cpp \
+    service_test.cpp \
     util_test.cpp \
 
 LOCAL_SHARED_LIBRARIES += \
