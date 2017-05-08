c2ef2f0d8d7b8b6c113e2fabb259209f9890f407
Tom Cherry <tomcherry@google.com>
2017-05-08 13:38:15 -0700

    init: remove restorecon() from util.cpp
    restorecon() has become nothing more than a small wrapper around
    selinux_android_restore().  This itself isn't super problematic, but
    it is an obstacle for compiling util.cpp on the host as that function
    is not available on the host.
    
    Bug: 36970783
    Test: Boot bullhead
    Change-Id: I7e209ece6898f9a0d5eb9e5d5d8155c2f1ba9faf

--- init/util.h
+++ init/util.h
@@ -68,7 +68,6 @@ int wait_for_file(const char *filename, std::chrono::nanoseconds timeout);
 void import_kernel_cmdline(bool in_qemu,
                            const std::function<void(const std::string&, const std::string&, bool)>&);
 int make_dir(const char* path, mode_t mode, selabel_handle* sehandle);
-int restorecon(const char *pathname, int flags = 0);
 std::string bytes_to_hex(const uint8_t *bytes, size_t bytes_len);
 bool is_dir(const char* pathname);
 bool expand_props(const std::string& src, std::string* dst);
