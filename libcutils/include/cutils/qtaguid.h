baede73bd92d8d0ae11e42c438fe2ca7f209e2c4
Chenbo Feng <fengc@google.com>
2017-10-25 12:31:43 -0700

    Redirect qtaguid native call to netd fwmark client
    In order to replace qtaguid module with new eBPF network monitoring
    module. We firstly move the current qtaguid userspace implementation
    into netd and hide the detail from other processes. The current API will
    talk to netd fwmark client to pass down the qtaguid related request from
    high level framework and netd will use the proper method to complete the
    request.
    
    Test: Current TrafficStats CTS tests should not fail.
    Bug: 30950746
    Change-Id: Ie90c28f3594ab2877746b2372a1b6944768bfb18

--- libcutils/include/cutils/qtaguid.h
+++ libcutils/include/cutils/qtaguid.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,49 +14,51 @@
  * limitations under the License.
  */
 
-#ifndef __LEGACY_QTAGUID_H
-#define __LEGACY_QTAGUID_H
+#ifndef __CUTILS_QTAGUID_H
+#define __CUTILS_QTAGUID_H
 
-#include <stdint.h>
 #include <sys/types.h>
-#include <unistd.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /*
- * Set tags (and owning UIDs) for network sockets. The socket must be untagged
- * by calling qtaguid_untagSocket() before closing it, otherwise the qtaguid
- * module will keep a reference to it even after close.
+ * Set tags (and owning UIDs) for network sockets.
  */
-extern int legacy_tagSocket(int sockfd, int tag, uid_t uid);
+extern int qtaguid_tagSocket(int sockfd, int tag, uid_t uid);
 
 /*
  * Untag a network socket before closing.
  */
-extern int legacy_untagSocket(int sockfd);
+extern int qtaguid_untagSocket(int sockfd);
 
 /*
  * For the given uid, switch counter sets.
  * The kernel only keeps a limited number of sets.
  * 2 for now.
  */
-extern int legacy_setCounterSet(int counterSetNum, uid_t uid);
+extern int qtaguid_setCounterSet(int counterSetNum, uid_t uid);
 
 /*
  * Delete all tag info that relates to the given tag an uid.
- * If the tag is 0, then ALL info about the uid is freeded.
- * The delete data also affects active tagged socketd, which are
+ * If the tag is 0, then ALL info about the uid is freed.
+ * The delete data also affects active tagged sockets, which are
  * then untagged.
  * The calling process can only operate on its own tags.
  * Unless it is part of the happy AID_NET_BW_ACCT group.
  * In which case it can clobber everything.
  */
-extern int legacy_deleteTagData(int tag, uid_t uid);
+extern int qtaguid_deleteTagData(int tag, uid_t uid);
+
+/*
+ * Enable/disable qtaguid functionnality at a lower level.
+ * When pacified, the kernel will accept commands but do nothing.
+ */
+extern int qtaguid_setPacifier(int on);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __LEGACY_QTAGUID_H */
+#endif /* __CUTILS_QTAG_UID_H */
