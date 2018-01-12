f8f09f53dd99e00f5d629c48daac6b8132aed088
faust93 <monumentum@gmail.com>
2018-01-12 12:13:42 +0800

    Shut up camera and wcnss debug output
    Former-commit-id: e9ce31cad6e33c18f0a6803cc3aa784a38aeca0e
    Former-commit-id: 8cfd4a11d47e8bec3c101789aee27bb978da9749
    Former-commit-id: 5bc635072c609395e8790fbdcea3acb279fb060a
    Former-commit-id: 148888055cc91dcf2da1565af7f8d13b82bedf7e
    Former-commit-id: 0b96d35c7e42809b7af0173a214866076d38a88d

--- liblog/logprint.c
+++ liblog/logprint.c
@@ -201,7 +201,7 @@ static android_LogPriority filterPriForTag(AndroidLogFormat* p_format,
 LIBLOG_ABI_PUBLIC int android_log_shouldPrintLine(AndroidLogFormat* p_format,
                                                   const char* tag,
                                                   android_LogPriority pri) {
-    if (!strncmp(tag, "AEC_PORT", 8) || !strncmp(tag, "mm-camera", 9) || !strncmp(tag, "GalleryDatab", 12))
+    if (!strncmp(tag, "QCamera", 7) || !strncmp(tag, "WCNSS_FILTER", 12) || !strncmp(tag, "mm-3a-core", 10) || !strncmp(tag, "AEC_PORT", 8) || !strncmp(tag, "mm-camera", 9) || !strncmp(tag, "GalleryDatab", 12))
         return 0;
     else
         return pri >= filterPriForTag(p_format, tag);
