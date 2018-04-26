master-hax/aosp-system-core

adb: Add a way to distinguish between connection failures and successes
This change adds a callback that is invoked exactly once, either when
the connection is fully established (i.e. CNXN packets have been sent
and received) or the atransport object is deleted before that (because
the connection failed).

This helps in distinguishing between successful and failing connections
for TCP. Especially when there is some kind of port
forwarding/multiplexing in between (like an SSH tunnel or SSLH proxy).

Bug: 74411879
Test: adb connect chromebook:22 (which runs an sslh tunnel to adbd).
      either succeeds or fails, but not fake-succeeds.

Change-Id: I7e826c6f5d4c30338a03b2d376a857ac5d05672a

adb/adb.cpp
@@ -132,6 +132,7 @@ void handle_online(atransport *t)
 {
     D("adb: online");
     t->online = 1;
+    t->RunConnectionCallbackOnce(true);
 }
 
 void handle_offline(atransport *t)

aosp-mirror/platform_system_core

adb/adb.cpp
@@ -132,6 +132,7 @@ void handle_online(atransport *t)
 {
     D("adb: online");
     t->online = 1;
+    t->SetConnectionEstablished(true);
 }
 
 void handle_offline(atransport *t)