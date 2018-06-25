a2df1ef4413144fe7d09a22f4750df05e83c13fb
Pirama Arumuga Nainar <pirama@google.com>
2018-06-25 11:48:58 -0700

    Do not customize __format__ for Windows/MinGW to gnu_printf
    Bug: http://b/69933090
    Bug: http://b/69933068
    
    MinGW uses gnu_printf to force C99 printf analysis with GCC, but clang
    does not support gnu_printf.  So just use the default that's used for
    other platforms.  This also mirrors upstream commit
    https://sourceforge.net/p/mingw-w64/mingw-w64/ci/015e637b4b24d9915162ab877ed539ad0e657951/.
    
    Test: m native-host-cross with Clang.
    Change-Id: I3deb266d70e25296c4ae1d58637afbabc3d949c1

--- fastboot/fastboot.h
+++ fastboot/fastboot.h
@@ -78,17 +78,8 @@ char* xstrdup(const char*);
 void set_verbose();
 
 // These printf-like functions are implemented in terms of vsnprintf, so they
-// use the same attribute for compile-time format string checking. On Windows,
-// if the mingw version of vsnprintf is used, use `gnu_printf' which allows z
-// in %zd and PRIu64 (and related) to be recognized by the compile-time
-// checking.
+// use the same attribute for compile-time format string checking.
 #define FASTBOOT_FORMAT_ARCHETYPE __printf__
-#ifdef __USE_MINGW_ANSI_STDIO
-#if __USE_MINGW_ANSI_STDIO
-#undef FASTBOOT_FORMAT_ARCHETYPE
-#define FASTBOOT_FORMAT_ARCHETYPE gnu_printf
-#endif
-#endif
 void die(const char* fmt, ...) __attribute__((__noreturn__))
 __attribute__((__format__(FASTBOOT_FORMAT_ARCHETYPE, 1, 2)));
 void verbose(const char* fmt, ...) __attribute__((__format__(FASTBOOT_FORMAT_ARCHETYPE, 1, 2)));
