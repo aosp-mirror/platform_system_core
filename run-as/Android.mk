ef62f3fcbb2189d230497f12996e25bae205245b
Luis Hector Chavez <lhchavez@google.com>
2018-06-27 10:40:10 -0700

    Add a way to disable run-as at runtime
    This change adds the ro.boot.disable_runas system property, that when
    set, disables the run-as command. This is done to reduce the surface
    area of programs that have file based capabilities in Chrome OS, and
    what they can do when running in non-developer mode.
    
    Bug: 31630024
    Test: run-as still works in aosp_sailfish
    Test: run-as still works in Android in Chrome OS (in developer mode)
    Change-Id: Iaf1d6f9ceb65081b7a9e17b9b91d8855e4080133

--- run-as/Android.mk
+++ run-as/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wall -Werror
 LOCAL_MODULE := run-as
-LOCAL_SHARED_LIBRARIES := libselinux libpackagelistparser libminijail
+LOCAL_SHARED_LIBRARIES := libbase libselinux libpackagelistparser libminijail
 LOCAL_SRC_FILES := run-as.cpp
 include $(BUILD_EXECUTABLE)
