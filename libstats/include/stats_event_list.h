5b63e2704a3d0dd23ff3ef9900639d5bb51aca61
Yao Chen <yaochen@google.com>
2018-10-02 10:51:49 -0700

    Add an API to release stats logger resources.
    Test: tested with ag/5112579
    Change-Id: I271b7574beab1baa9886d31bc1cf25c40c2fae23

--- libstats/include/stats_event_list.h
+++ libstats/include/stats_event_list.h
@@ -25,6 +25,7 @@ extern "C" {
 void reset_log_context(android_log_context ctx);
 int write_to_logger(android_log_context context, log_id_t id);
 void note_log_drop();
+void stats_log_close();
 
 #ifdef __cplusplus
 }
