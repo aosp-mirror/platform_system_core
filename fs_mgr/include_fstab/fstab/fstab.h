0b721d3dd9ba8c3b502bd6be90c6fb8b39a5bddf
Leo Liou <leoliou@google.com>
2019-01-15 20:43:37 +0800

    fs_mgr: set ext4 fsverity feature bit
    It supports a new feature "fsverity" for ext4 file-system. When someone
    adds a mount option "fsverity" to fstab file, fsmgr will use tune2fs to
    enable fsverity feature if kernel support.
    
    Bug: 117437571
    Test: bootable for phone projects
    Change-Id: Icf35715a2f71c430468daaafcde497f8cfe9a18d

--- fs_mgr/include_fstab/fstab/fstab.h
+++ fs_mgr/include_fstab/fstab/fstab.h
@@ -95,6 +95,7 @@ int fs_mgr_is_checkpoint(const struct fstab_rec* fstab);
 int fs_mgr_is_checkpoint_fs(const struct fstab_rec* fstab);
 int fs_mgr_is_checkpoint_blk(const struct fstab_rec* fstab);
 int fs_mgr_has_sysfs_path(const struct fstab_rec* fstab);
+int fs_mgr_is_fs_verity(const struct fstab_rec* fstab);
 
 std::string fs_mgr_get_slot_suffix();
 std::set<std::string> fs_mgr_get_boot_devices();
@@ -177,6 +178,7 @@ struct FstabEntry {
             bool zram_loopback_path : 1;
             bool zram_loopback_size : 1;
             bool zram_backing_dev_path : 1;
+            bool fs_verity : 1;
         };
     } fs_mgr_flags;
 
