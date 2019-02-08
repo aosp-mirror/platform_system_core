98a61dd583ab7f78b2de0fb38a2edb453d6e208a
Yifan Hong <elsk@google.com>
2019-02-08 12:34:48 -0800

    Move rootdir/*.json to libprocessgroup
    They are installed to /system/etc, not /. Hence, move
    out of rootdir/ and move into the code that parse these
    files (i.e. libprocessgroup).
    
    Also, Android.mk->bp.
    
    Test: builds
    
    Change-Id: I1763c26f81ae51ee159552e39d252fd643c35b94

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -64,38 +64,6 @@ $(foreach b,$(ART_BINARIES), \
 include $(BUILD_PREBUILT)
 
 #######################################
-# cgroups.json
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := cgroups.json
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-
-include $(BUILD_PREBUILT)
-
-#######################################
-# cgroups.json for recovery
-include $(CLEAR_VARS)
-LOCAL_MODULE := cgroups.recovery.json
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/etc
-LOCAL_MODULE_STEM := cgroups.json
-include $(BUILD_PREBUILT)
-
-#######################################
-# task_profiles.json
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := task_profiles.json
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
-
-include $(BUILD_PREBUILT)
-
-#######################################
 # asan.options
 ifneq ($(filter address,$(SANITIZE_TARGET)),)
 
