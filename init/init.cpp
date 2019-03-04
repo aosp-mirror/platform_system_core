1dacd42ae189c685ac9e5bea1c25a845580f67d5
Bowgo Tsai <bowgotsai@google.com>
2019-03-04 17:53:34 +0800

    Allow overriding ro.debuggable to 1 on USER builds
    When init found "/force_debuggable" in the first-stage ramdisk, it will
    do the following if the device is unlocked:
      1. load /system/etc/adb_debug.prop (with ro.debuggable=1)
      2 .load userdebug_plat_sepolicy.cil instead of original plat_sepolicy.cil from
        /system/etc/selinux/.
    
    This make it possible to run VTS on a USER build GSI, by using a special
    ramdisk containing "/force_debuggable".
    
    Bug: 126493225
    Test: unlock a USER build device, check 'adb root' can work
    Change-Id: I9b4317bac1ce92f2c0baa67c83d4b12deba62c92

--- init/init.cpp
+++ init/init.cpp
@@ -39,6 +39,7 @@
 #include <android-base/stringprintf.h>
 #include <android-base/strings.h>
 #include <cutils/android_reboot.h>
+#include <fs_avb/fs_avb.h>
 #include <fs_mgr_vendor_overlay.h>
 #include <keyutils.h>
 #include <libavb/libavb.h>
@@ -74,6 +75,7 @@ using android::base::ReadFileToString;
 using android::base::StringPrintf;
 using android::base::Timer;
 using android::base::Trim;
+using android::fs_mgr::AvbHandle;
 
 namespace android {
 namespace init {
@@ -92,6 +94,7 @@ static std::string wait_prop_value;
 static bool shutting_down;
 static std::string shutdown_command;
 static bool do_shutdown = false;
+static bool load_debug_prop = false;
 
 std::vector<std::string> late_import_paths;
 
@@ -655,10 +658,17 @@ int SecondStageMain(int argc, char** argv) {
     const char* avb_version = getenv("INIT_AVB_VERSION");
     if (avb_version) property_set("ro.boot.avb_version", avb_version);
 
+    // See if need to load debug props to allow adb root, when the device is unlocked.
+    const char* force_debuggable_env = getenv("INIT_FORCE_DEBUGGABLE");
+    if (force_debuggable_env && AvbHandle::IsDeviceUnlocked()) {
+        load_debug_prop = "true"s == force_debuggable_env;
+    }
+
     // Clean up our environment.
     unsetenv("INIT_STARTED_AT");
     unsetenv("INIT_SELINUX_TOOK");
     unsetenv("INIT_AVB_VERSION");
+    unsetenv("INIT_FORCE_DEBUGGABLE");
 
     // Now set up SELinux for second stage.
     SelinuxSetupKernelLogging();
@@ -672,7 +682,7 @@ int SecondStageMain(int argc, char** argv) {
 
     InstallSignalFdHandler(&epoll);
 
-    property_load_boot_defaults();
+    property_load_boot_defaults(load_debug_prop);
     fs_mgr_vendor_overlay_mount_all();
     export_oem_lock_status();
     StartPropertyService(&epoll);
