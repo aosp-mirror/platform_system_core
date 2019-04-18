93d63c010ae24ff58acb70216bfcb93e3c73019a
Josh Gao <jmgao@google.com>
2019-04-18 16:39:30 -0700

    Add a way to turn off unique_fd's operator int.
    unique_fd's implicit conversion to int has led to tons of problems (see
    all of the overloads for close, fdopen, fdopendir, etc.). Add a switch
    that can turn it off, and reduce the ridiculous amount of work to fix up
    callers by introducing a borrowed_fd type that can be constructed from
    either int or unique_fd.
    
    Test: treehugger
    Change-Id: If77cf5cbcaddacdaec5919a15b3520fb68f51a62

--- adb/daemon/shell_service_test.cpp
+++ adb/daemon/shell_service_test.cpp
@@ -77,7 +77,7 @@ void ShellServiceTest::StartTestCommandInProcess(std::string name, Command comma
 namespace {
 
 // Reads raw data from |fd| until it closes or errors.
-std::string ReadRaw(int fd) {
+std::string ReadRaw(borrowed_fd fd) {
     char buffer[1024];
     char *cur_ptr = buffer, *end_ptr = buffer + sizeof(buffer);
 
@@ -93,12 +93,12 @@ std::string ReadRaw(int fd) {
 // Reads shell protocol data from |fd| until it closes or errors. Fills
 // |stdout| and |stderr| with their respective data, and returns the exit code
 // read from the protocol or -1 if an exit code packet was not received.
-int ReadShellProtocol(int fd, std::string* stdout, std::string* stderr) {
+int ReadShellProtocol(borrowed_fd fd, std::string* stdout, std::string* stderr) {
     int exit_code = -1;
     stdout->clear();
     stderr->clear();
 
-    auto protocol = std::make_unique<ShellProtocol>(fd);
+    auto protocol = std::make_unique<ShellProtocol>(fd.get());
     while (protocol->Read()) {
         switch (protocol->id()) {
             case ShellProtocol::kIdStdout:
