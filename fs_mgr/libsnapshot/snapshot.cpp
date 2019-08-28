caaf9a617af7bacaf735586cf6e7ca9a13d7d2ab
Yifan Hong <elsk@google.com>
2019-08-27 18:56:07 -0700

    libsnapshot: require ex lock for {Create}{CowImage,Snapshot} / DeleteSnapshot
    Create / Deleting the COW image / snapshot changes states, so it makes
    sense to require an exclusive lock before doing so. If caller doesn't hold
    an exclusive lock, parallel calls to MapCowImage / MapSnapshot / UnmapCowImage /
    UnmapSnapshot may have weird results.
    
    Test: libsnapshot_test
    
    Change-Id: I4be660df1059ec24144f8baf43a1c8c05d9e372b

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -183,6 +183,7 @@ bool SnapshotManager::FinishedSnapshotWrites() {
 bool SnapshotManager::CreateSnapshot(LockedFile* lock, const std::string& name,
                                      SnapshotManager::SnapshotStatus status) {
     CHECK(lock);
+    CHECK(lock->lock_mode() == LOCK_EX);
     // Sanity check these sizes. Like liblp, we guarantee the partition size
     // is respected, which means it has to be sector-aligned. (This guarantee
     // is useful for locating avb footers correctly). The COW size, however,
@@ -215,6 +216,7 @@ bool SnapshotManager::CreateSnapshot(LockedFile* lock, const std::string& name,
 
 bool SnapshotManager::CreateCowImage(LockedFile* lock, const std::string& name) {
     CHECK(lock);
+    CHECK(lock->lock_mode() == LOCK_EX);
     if (!EnsureImageManager()) return false;
 
     SnapshotStatus status;
@@ -415,6 +417,7 @@ bool SnapshotManager::UnmapCowImage(const std::string& name) {
 
 bool SnapshotManager::DeleteSnapshot(LockedFile* lock, const std::string& name) {
     CHECK(lock);
+    CHECK(lock->lock_mode() == LOCK_EX);
     if (!EnsureImageManager()) return false;
 
     auto cow_image_name = GetCowImageDeviceName(name);
