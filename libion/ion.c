d43e79855301e3039a88bace941c66b02645f451
Hridya Valsaraju <hridya@google.com>
2019-09-06 12:20:32 -0700

    Add libion API to check if modular heaps are being used
    Bug: 140916230
    Test: ./ion-unit-tests
    
    Change-Id: Iabb050a70d9a838f2720c509df4017d6031ce1fe

--- libion/ion.c
+++ libion/ion.c
@@ -35,6 +35,8 @@
 
 #include <log/log.h>
 
+#define ION_ABI_VERSION_MODULAR_HEAPS 2
+
 enum ion_version { ION_VERSION_UNKNOWN, ION_VERSION_MODERN, ION_VERSION_LEGACY };
 
 static atomic_int g_ion_version = ATOMIC_VAR_INIT(ION_VERSION_UNKNOWN);
@@ -73,6 +75,14 @@ static int ion_ioctl(int fd, int req, void* arg) {
         return -errno;
     }
     return ret;
+}
+
+int ion_is_using_modular_heaps(int fd) {
+    int ion_abi_version = 0;
+    int ret = 0;
+
+    ret = ion_ioctl(fd, ION_IOC_ABI_VERSION, &ion_abi_version);
+    return (ret == 0 && ion_abi_version >= ION_ABI_VERSION_MODULAR_HEAPS);
 }
 
 int ion_alloc(int fd, size_t len, size_t align, unsigned int heap_mask, unsigned int flags,
