cb0dfbf14ee08f904dcce47a2b4e06965eb0d88b
Steven Moreland <smoreland@google.com>
2019-10-07 17:31:17 -0700

    FlattenableUtils::align memsets
    Bug: 141890807
    Test: boot, check data is zero'd
    Change-Id: I45aaeac369f4c5cf3eb44f61c233e00f870a5c79
    (cherry picked from commit bf824f8fa50d9f7390057278fefb58e83323a644)
    (cherry picked from commit e62a9d7669ac85844d4424474f5c88c096f7160b)

--- libutils/include/utils/Flattenable.h
+++ libutils/include/utils/Flattenable.h
@@ -47,7 +47,12 @@ public:
 
     template<size_t N>
     static size_t align(void*& buffer) {
-        return align<N>( const_cast<void const*&>(buffer) );
+        static_assert(!(N & (N - 1)), "Can only align to a power of 2.");
+        void* b = buffer;
+        buffer = reinterpret_cast<void*>((uintptr_t(buffer) + (N-1)) & ~(N-1));
+        size_t delta = size_t(uintptr_t(buffer) - uintptr_t(b));
+        memset(b, 0, delta);
+        return delta;
     }
 
     static void advance(void*& buffer, size_t& size, size_t offset) {
