b8bacea8183447c2e2cb817e1ed18ce9d80349d7
Tom Cherry <tomcherry@google.com>
2019-11-15 16:41:36 -0800

    liblog: return 0 from android_logger_list_read() when recv() returns 0
    We used to do this, but it got lost while refactoring this code.
    
    Bug: 144311420
    Test: we see "unexpected EOF!" instead of "unexpected length" from logcat
    Change-Id: I7858d0a774a9eac63e5547ee67e85ef8fb0c682d

--- liblog/logger_read.cpp
+++ liblog/logger_read.cpp
@@ -272,7 +272,7 @@ static int android_transport_read(struct android_log_logger_list* logger_list,
                                   struct log_msg* log_msg) {
   int ret = (*transp->transport->read)(logger_list, transp, log_msg);
 
-  if (ret < 0) {
+  if (ret <= 0) {
     return ret;
   }
 
