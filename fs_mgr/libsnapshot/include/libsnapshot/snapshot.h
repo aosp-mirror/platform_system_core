c7c7a21d2eaab5d4acdc8139a73828d17556cce6
Yifan Hong <elsk@google.com>
2020-03-24 10:57:56 -0700

    libsnapshot: Place forward merge indicator for wipe
    If the device applies a package with FDR, place forward merge indicator
    so that on FDR, the snapshots aren't abandoned, and we forward merge
    the update.
    
    Test: apply downgrade
    Test: apply update with data wipe
    Test: vts_libsnapshot_test
    Bug: 152094219
    Change-Id: Id32aeca380db797cd777321837f146333ae6b33c
    (cherry picked from commit 6b0251232ab368b52c67825a5edad568c9afeab7)
    Merged-In: Id32aeca380db797cd777321837f146333ae6b33c

--- fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
+++ fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
@@ -142,7 +142,9 @@ class SnapshotManager final {
     // be created, and the device must either cancel the OTA (either before
     // rebooting or after rolling back), or merge the OTA.
     // Before calling this function, all snapshots must be mapped.
-    bool FinishedSnapshotWrites();
+    // If |wipe| is set to true, wipe is scheduled after reboot, and snapshots
+    // may need to be merged before wiping.
+    bool FinishedSnapshotWrites(bool wipe);
 
     // Initiate a merge on all snapshot devices. This should only be used after an
     // update has been marked successful after booting.
@@ -522,6 +524,10 @@ class SnapshotManager final {
     // Check whether |name| should be deleted as a snapshot name.
     bool ShouldDeleteSnapshot(LockedFile* lock, const std::map<std::string, bool>& flashing_status,
                               Slot current_slot, const std::string& name);
+
+    // Create or delete forward merge indicator given |wipe|. Iff wipe is scheduled,
+    // allow forward merge on FDR.
+    bool UpdateForwardMergeIndicator(bool wipe);
 
     std::string gsid_dir_;
     std::string metadata_dir_;
