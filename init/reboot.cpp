860ba643930119c2a54b70184f234a5a2b5386e3
Martijn Coenen <maco@google.com>
2020-05-28 16:42:10 +0200

    Abort FUSE filesystems during shutdown.
    To ensure we can shutdown cleanly, and don't hang an outstanding
    requests to a FUSE host daemon that has already exited.
    
    Bug: 153411204
    Test: inspect logs during shutdown
    Change-Id: I8e6479bd54dbc1fc85b087617aa6b16be9f15a3b

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -678,9 +678,12 @@ static void DoReboot(unsigned int cmd, const std::string& reason, const std::str
     // Reap subcontext pids.
     ReapAnyOutstandingChildren();
 
-    // 3. send volume shutdown to vold
+    // 3. send volume abort_fuse and volume shutdown to vold
     Service* vold_service = ServiceList::GetInstance().FindService("vold");
     if (vold_service != nullptr && vold_service->IsRunning()) {
+        // Manually abort FUSE connections, since the FUSE daemon is already dead
+        // at this point, and unmounting it might hang.
+        CallVdc("volume", "abort_fuse");
         CallVdc("volume", "shutdown");
         vold_service->Stop();
     } else {
