b7e3d9579894fc134bea1ab62428990339fa0ca2
Mark Salyzyn <salyzyn@google.com>
2020-11-20 09:30:29 -0800

    storaged: protect global proto_loaded
    proto_loaded is not thread safe, so we must protect it with
    a mutex proto_lock.
    
    Signed-off-by: hanhongli@360.cn
    Signed-off-by: gongguang@360.cn
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 170732441
    Test: compile
    Change-Id: I93416598b6047e890c66b922b360093aa42db8c5
    (cherry picked from commit 4913d3d760af98765a40a8642624014177290958)

--- storaged/storaged.cpp
+++ storaged/storaged.cpp
@@ -164,6 +164,8 @@ storaged_t::storaged_t(void) {
 }
 
 void storaged_t::add_user_ce(userid_t user_id) {
+    Mutex::Autolock _l(proto_lock);
+
     if (!proto_loaded[user_id]) {
         load_proto(user_id);
         proto_loaded[user_id] = true;
@@ -171,6 +173,8 @@ void storaged_t::add_user_ce(userid_t user_id) {
 }
 
 void storaged_t::remove_user_ce(userid_t user_id) {
+    Mutex::Autolock _l(proto_lock);
+
     proto_loaded[user_id] = false;
     mUidm.clear_user_history(user_id);
     RemoveFileIfExists(proto_path(user_id), nullptr);
@@ -301,6 +305,8 @@ void storaged_t::flush_proto(userid_t user_id, StoragedProto* proto) {
 }
 
 void storaged_t::flush_protos(unordered_map<int, StoragedProto>* protos) {
+    Mutex::Autolock _l(proto_lock);
+
     for (auto& it : *protos) {
         /*
          * Don't flush proto if we haven't attempted to load it from file.
