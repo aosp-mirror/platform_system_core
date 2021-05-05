d926aded7370b97847a25045ec4eae4a252cc228
David Ng <dng@quicinc.com>
2021-05-05 09:30:18 -0700

    OverlayFS support for fstab
    Add overlayfs (lowerdir) mount entry support to fstab.
    
      overlay  <final dir>  overlay lowerdir=/1:/2
    
    E.g.
      overlay /vendor overlay lowerdir=/odm/vnd_ovl1/1:/odm/vnd_ovl2
    
    Test: Ensure mounting with fstab overlayfs entry
    Change-Id: Ib025e203f8ac1836ab62dfa96fb14e8e108f82fb

--- fs_mgr/include/fs_mgr_overlayfs.h
+++ fs_mgr/include/fs_mgr_overlayfs.h
@@ -27,6 +27,7 @@
 android::fs_mgr::Fstab fs_mgr_overlayfs_candidate_list(const android::fs_mgr::Fstab& fstab);
 
 bool fs_mgr_overlayfs_mount_all(android::fs_mgr::Fstab* fstab);
+bool fs_mgr_overlayfs_mount_fstab_entry (const std::string& lowers, const std::string& mount_point);
 std::vector<std::string> fs_mgr_overlayfs_required_devices(android::fs_mgr::Fstab* fstab);
 bool fs_mgr_overlayfs_setup(const char* backing = nullptr, const char* mount_point = nullptr,
                             bool* change = nullptr, bool force = true);
