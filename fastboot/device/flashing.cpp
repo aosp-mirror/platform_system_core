0cf395cee09d1be1046d60704599b7c6653e2034
Keith Mok <keithmok@google.com>
2021-12-31 05:09:32 +0000

    Add checking for sparse file format
    Sparse file can come from an untrusted source.
    Need more checking to ensure that it is not a malformed
    file and would not cause any OOB read access.
    
    Update fuzz test for decoding also.
    
    Ignore-AOSP-First: Awaiting security triage
    Test: adb reboot fastboot
          fuzzy_fastboot --gtest_filter=Fuzz.Sparse*
          fuzzy_fastboot --gtest_filter=Conformance.Sparse*
          sparse_fuzzer
    Bug: 212705418
    Change-Id: I7622df307bb00e59faaba8bb2c67cb474cffed8e

--- fastboot/device/flashing.cpp
+++ fastboot/device/flashing.cpp
@@ -119,9 +119,11 @@ int WriteCallback(void* priv, const void* data, size_t len) {
 }
 
 int FlashSparseData(int fd, std::vector<char>& downloaded_data) {
-    struct sparse_file* file = sparse_file_import_buf(downloaded_data.data(), true, false);
+    struct sparse_file* file = sparse_file_import_buf(downloaded_data.data(),
+                                                      downloaded_data.size(), true, false);
     if (!file) {
-        return -ENOENT;
+        // Invalid sparse format
+        return -EINVAL;
     }
     return sparse_file_callback(file, false, false, WriteCallback, reinterpret_cast<void*>(fd));
 }
