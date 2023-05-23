791a83bb4565b3b2a4e3d40ea2c0ec592cfb0d04
Daniel Zheng <zhengdaniel@google.com>
2023-05-23 10:42:39 -0700

    Chang flash task to take in source
    Old changes didn't actually take into account sources. fastboot update
    {update.zip} should read fastboot-info from the update package. It was
    instead flashing local images + reading local fastboot-info.txt
    
    Test: m fastboot, fastboot update, fastboot flashall
    Bug: 283330320
    
    Change-Id: I9587a7735ddfbfb661153eb12ebc3caa7c32f141

--- fastboot/task.h
+++ fastboot/task.h
@@ -46,7 +46,7 @@ class Task {
 class FlashTask : public Task {
   public:
     FlashTask(const std::string& slot, const std::string& pname, const std::string& fname,
-              const bool apply_vbmeta);
+              const bool apply_vbmeta, const FlashingPlan* fp);
     virtual FlashTask* AsFlashTask() override { return this; }
 
     std::string GetPartition() { return pname_; }
@@ -60,6 +60,7 @@ class FlashTask : public Task {
     const std::string fname_;
     const std::string slot_;
     const bool apply_vbmeta_;
+    const FlashingPlan* fp_;
 };
 
 class RebootTask : public Task {
