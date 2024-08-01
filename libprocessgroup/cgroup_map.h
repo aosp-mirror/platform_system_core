fcb8666eed813bc3ddfd080f0dfb7ffdb1011787
T.J. Mercier <tjmercier@google.com>
2024-08-01 20:52:30 +0000

    libprocessgroup: Rename CgroupController -> CgroupControllerWrapper
    So that the name is not overloaded with libcgrouprc_format's
    CgroupController, which can be confusing.
    
    Bug: 349105928
    Test: m
    Change-Id: I39df9814c500de68fd20139e661363ba51ea3543

--- libprocessgroup/cgroup_map.h
+++ libprocessgroup/cgroup_map.h
@@ -23,10 +23,10 @@
 #include <android/cgrouprc.h>
 
 // Convenient wrapper of an ACgroupController pointer.
-class CgroupController {
+class CgroupControllerWrapper {
   public:
     // Does not own controller
-    explicit CgroupController(const ACgroupController* controller)
+    explicit CgroupControllerWrapper(const ACgroupController* controller)
         : controller_(controller) {}
 
     uint32_t version() const;
@@ -57,8 +57,8 @@ class CgroupMap {
     static bool SetupCgroups();
 
     static CgroupMap& GetInstance();
-    CgroupController FindController(const std::string& name) const;
-    CgroupController FindControllerByPath(const std::string& path) const;
+    CgroupControllerWrapper FindController(const std::string& name) const;
+    CgroupControllerWrapper FindControllerByPath(const std::string& path) const;
     int ActivateControllers(const std::string& path) const;
 
   private:
