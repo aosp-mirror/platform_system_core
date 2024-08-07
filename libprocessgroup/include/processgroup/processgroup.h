b6071f19c318fd19ed37d9fa4ba0cf77d7d32d13
T.J. Mercier <tjmercier@google.com>
2024-08-07 16:35:55 +0000

    libprocessgroup: Convert CGROUPV2_HIERARCHY_NAME to std::string
    Almost everywhere CGROUPV2_HIERARCHY_NAME is used a std::string is
    required, so change its type to avoid temporaries.
    
    Change-Id: I4466838c510f2eb8212fc71999cdaa47359ce9c3

--- libprocessgroup/include/processgroup/processgroup.h
+++ libprocessgroup/include/processgroup/processgroup.h
@@ -26,7 +26,7 @@
 
 __BEGIN_DECLS
 
-static constexpr const char* CGROUPV2_HIERARCHY_NAME = "cgroup2";
+static constexpr std::string CGROUPV2_HIERARCHY_NAME = "cgroup2";
 
 bool CgroupsAvailable();
 bool CgroupGetControllerPath(const std::string& cgroup_name, std::string* path);
