bb4a40955aa8c1350c6f2dc1ac5a0ade7bcab724
Eric Caruso <ejcaruso@google.com>
2024-08-30 13:34:24 -0400

    ueventd: fix other assorted style nits
    Indentation in this file is four spaces, remove an
    unnecessary trailing semicolon, put a space between
    a type declaration and its structured binding.
    
    clang-format shuffled a line around as well.
    
    Bug: None
    Test: compile
    Change-Id: Ib4cf17fecb1e54971020dc77b7903d2aac5dd9c1

--- init/devices.cpp
+++ init/devices.cpp
@@ -283,7 +283,7 @@ std::tuple<mode_t, uid_t, gid_t> DeviceHandler::GetDevicePermissions(
 
 void DeviceHandler::MakeDevice(const std::string& path, bool block, int major, int minor,
                                const std::vector<std::string>& links) const {
-    auto[mode, uid, gid] = GetDevicePermissions(path, links);
+    auto [mode, uid, gid] = GetDevicePermissions(path, links);
     mode |= (block ? S_IFBLK : S_IFCHR);
 
     std::string secontext;
@@ -531,7 +531,7 @@ void DeviceHandler::HandleAshmemUevent(const Uevent& uevent) {
         if (!ReadFileToString(boot_id_path, &boot_id)) {
             PLOG(ERROR) << "Cannot duplicate ashmem device node. Failed to read " << boot_id_path;
             return;
-        };
+        }
         boot_id = Trim(boot_id);
 
         Uevent dup_ashmem_uevent = uevent;
@@ -542,10 +542,10 @@ void DeviceHandler::HandleAshmemUevent(const Uevent& uevent) {
 }
 
 void DeviceHandler::HandleUevent(const Uevent& uevent) {
-  if (uevent.action == "add" || uevent.action == "change" ||
-      uevent.action == "bind" || uevent.action == "online") {
-    FixupSysPermissions(uevent.path, uevent.subsystem);
-  }
+    if (uevent.action == "add" || uevent.action == "change" || uevent.action == "bind" ||
+        uevent.action == "online") {
+        FixupSysPermissions(uevent.path, uevent.subsystem);
+    }
 
     // if it's not a /dev device, nothing to do
     if (uevent.major < 0 || uevent.minor < 0) return;
