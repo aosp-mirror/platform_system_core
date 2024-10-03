36ea62f1fd9dda7c733c229e84ab1da37db370c2
Tiffany Yang <ynaffit@google.com>
2024-10-03 23:52:54 +0000

    Revert "init: Wait for /dev/hvc1 during ARCVM first-stage mount"
    This reverts commit b885e4ad533ea4d12e18a0147d298be0cd8cec72.
    
    Reason for revert: No longer needed (using virtio-blk instead)
    
    Change-Id: I667f0d4f58060e781a5e9b21c778cd568b92971a

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -305,11 +305,6 @@ bool FirstStageMountVBootV2::InitDevices() {
             return false;
         }
     }
-
-    if (IsArcvm() && !block_dev_init_.InitHvcDevice("hvc1")) {
-        return false;
-    }
-
     return true;
 }
 
